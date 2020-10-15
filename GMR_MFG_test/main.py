ce5096abda49e29de009415e438eb7e074341d3e
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2020-10-15 17:20:04 +0800

    print length
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- GMR_MFG_test/main.py
+++ GMR_MFG_test/main.py
@@ -1,26 +1,30 @@
 import json
 import pandas as pd
 import numpy as np
+import random
+
+seed = 1234
+random.seed(seed)
+
 items = ['pbe', 'hse', 'gllb-sc', 'scan']
 
 structures = []
 targets = []
-
-from pymatgen import MPRester
-
-with MPRester("zBbeX4qitlXrl2uBfIP") as mpr:
-    structure = mpr.get_structure_by_material_id('mp-1143')
-    print(type(structure))
+data_size = []
 
 from pymatgen.core.structure import Structure
 
 for it in items:
     csv_name = 'data/' + it + '_cif.csv'
     df = pd.read_csv(csv_name)
+    data_size.append(len(df))
     for i in range(len(df)):
-        # structures.append(dict(df[it+'_structure'][i]))
         structures.append(Structure.from_str(df[it+'_structure'][i], fmt='cif'))
         targets.append(df[it+'_gap'][i])
+
+print('4 data size is:', data_size)
+
+### load exp data and shuffle
 
 from megnet.data.crystal import CrystalGraph
 from megnet.data.graph import GaussianDistance
@@ -31,3 +35,4 @@ model = MEGNetModel(10, 2, nblocks=1, lr=1e-2,
         graph_converter=CrystalGraph(bond_converter=GaussianDistance(np.linspace(0, 5, 10), 0.5)))
 
 model.train(structures, targets, epochs=2)
+
