5b85b5609ac6afb5579c5b14902eca127b00dff9
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2020-10-15 11:08:27 +0800

    add download script
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

---
+++ GMR_MFG_test/download.py
@@ -0,0 +1,38 @@
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data_path = 'data/all_data.json' # put here the path to the json file
+with open(data_path,'r') as fp:
+    d = json.load(fp)
+## load structure from mp-id
+
+from pymatgen.ext.matproj import MPRester
+from pymatgen.io.cif import CifParser
+
+structures = []
+pbe_keys = list(d['pbe'].keys())
+print(len(pbe_keys))
+batch_size = 1024
+
+with MPRester("zBbeX4qitlXrl2uBfIP") as mpr:
+    # if batch_size > 0:
+    #     for i in range(int(len(pbe_keys)/batch_size)):
+    #         lst = mpr.get_structures(pbe_keys[i*batch_size:min((i+1)*batch_size, len(pbe_keys))])
+    #         structures += lst
+    #         print(len(structures))
+    # else:
+    for i in range(len(pbe_keys)):
+        s1 = mpr.get_structure_by_material_id(pbe_keys[i])
+        # or
+        # data = mpr.query(criteria={"task_id": pbe_keys[i]}, properties=["final_energy", "cif"])
+        # s2 = CifParser.from_string(data[0]["cif"]).get_structures()
+        structures.append(s1)
+        if len(structures)%10 == 0:
+            print(len(structures))
+
+# dump the data
+df_pbe = pd.DataFrame({'mp_id':list(d['pbe'].keys()), 'pbe_gap':list(d['pbe'].values()), 'pbe_structure':structures})
+df_pbe.set_index('mp_id',inplace=True)
+df_pbe.to_csv('data/pbe.csv')
+
