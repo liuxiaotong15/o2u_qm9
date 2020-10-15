c8470df521732457fa5a7a7166bf500cb9c4e205
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2020-10-16 06:13:05 +0800

    change a retry method
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- GMR_MFG_test/download.py
+++ GMR_MFG_test/download.py
@@ -20,26 +20,27 @@ pbe_keys = list(d[task].keys())
 print(len(pbe_keys))
 batch_size = 1024
 
-with MPRester("zBbeX4qitlXrl2uBfIP") as mpr:
-    # if batch_size > 0:
-    #     for i in range(int(len(pbe_keys)/batch_size)):
-    #         lst = mpr.get_structures(pbe_keys[i*batch_size:min((i+1)*batch_size, len(pbe_keys))])
-    #         structures += lst
-    #         print(len(structures))
-    # else:
-    for i in range(len(pbe_keys)):
-        try:
-            s1 = mpr.get_structure_by_material_id(pbe_keys[i])
-            # or
-            # data = mpr.query(criteria={"task_id": pbe_keys[i]}, properties=["final_energy", "cif"])
-            # s2 = CifParser.from_string(data[0]["cif"]).get_structures()
-        except:
-            print('something wrong with index:', i, 'mp id is: ', pbe_keys[i])
-            i -= 1
-        else:
-            structures.append(s1.to(fmt="cif"))
-            if len(structures)%10 == 0:
-                print(len(structures))
+while len(structures) < len(pbe_keys):
+    with MPRester("zBbeX4qitlXrl2uBfIP") as mpr:
+        # if batch_size > 0:
+        #     for i in range(int(len(pbe_keys)/batch_size)):
+        #         lst = mpr.get_structures(pbe_keys[i*batch_size:min((i+1)*batch_size, len(pbe_keys))])
+        #         structures += lst
+        #         print(len(structures))
+        # else:
+        for i in range(len(pbe_keys)):
+            try:
+                s1 = mpr.get_structure_by_material_id(pbe_keys[i])
+                # or
+                # data = mpr.query(criteria={"task_id": pbe_keys[i]}, properties=["final_energy", "cif"])
+                # s2 = CifParser.from_string(data[0]["cif"]).get_structures()
+            except:
+                print('something wrong with index:', i, 'mp id is: ', pbe_keys[i])
+                break
+            else:
+                structures.append(s1.to(fmt="cif"))
+                if len(structures)%10 == 0:
+                    print(len(structures))
 
 
 # dump the data
