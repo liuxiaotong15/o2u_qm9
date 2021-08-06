d7908551ecf627402998b4aa6e9c2177180cbabd
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2021-08-06 11:25:59 +0800

    add mode 11 to use 20% of E1 as validation dataset
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- MFG_reproduce_202108/main.py
+++ MFG_reproduce_202108/main.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 import os
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
  
 
 seed = 1234
@@ -176,12 +176,30 @@ elif training_mode == 6: # PBE -> HSE ... -> part EXP, one by one, with 20% vali
         model.train(structures[idx:idx+int(0.8*data_size[i])], targets[idx:idx+int(0.8*data_size[i])],
                 validation_structures=structures[idx+int(0.8*data_size[i]):(idx+data_size[i])],
                 validation_targets=targets[idx+int(0.8*data_size[i]):(idx+data_size[i])],
-                callbacks=[callback, XiaotongCB((test_input, test_targets), commit_id)],
+                # callbacks=[callback, XiaotongCB((test_input, test_targets), commit_id)],
+                callbacks=[callback],
                 epochs=ep,
                 save_checkpoint=False,
                 batch_size = 512,
                 automatic_correction=False)
-        model.save_model(commit_id+'_'+str(training_mode)+'_'+str(i)+'.hdf5')
+        # model.save_model(commit_id+'_'+str(training_mode)+'_'+str(i)+'.hdf5')
+        idx += data_size[i]
+        prediction(model)
+elif training_mode == 11: # PBE -> HSE ... -> part EXP, one by one, with 20% of last dataset as validation
+    idx = 0
+    for i in range(len(data_size)):
+        model.train(structures[idx:idx+int(0.8*data_size[i])], targets[idx:idx+int(0.8*data_size[i])],
+                validation_structures=structures[sum(data_size[:-1])+int(0.8*data_size[-1]:],
+                validation_targets=targets[sum(data_size[:-1])+int(0.8*data_size[-1]:],
+                # validation_structures=structures[idx+int(0.8*data_size[i]):(idx+data_size[i])],
+                # validation_targets=targets[idx+int(0.8*data_size[i]):(idx+data_size[i])],
+                # callbacks=[callback, XiaotongCB((test_input, test_targets), commit_id)],
+                callbacks=[callback],
+                epochs=ep,
+                save_checkpoint=False,
+                batch_size = 512,
+                automatic_correction=False)
+        # model.save_model(commit_id+'_'+str(training_mode)+'_'+str(i)+'.hdf5')
         idx += data_size[i]
         prediction(model)
 elif training_mode == 7: # all training set together with 20% validation
