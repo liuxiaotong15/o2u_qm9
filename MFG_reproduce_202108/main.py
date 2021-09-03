6de0b2a38c68c6dbb4df9863cf057ac83cf31e05
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2021-09-03 19:39:29 +0800

    add cif dump in prediction of E2
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- MFG_reproduce_202108/main.py
+++ MFG_reproduce_202108/main.py
@@ -16,7 +16,16 @@ from megnet.callbacks import XiaotongCB
 import sys
 training_mode = int(sys.argv[1])
 seed = 123
-GPU_device = "0"
+GPU_device = ""
+dump_prediction_cif = True
+load_old_model_enable = True
+predict_before_dataclean = True
+training_new_model = False
+
+# old_model_name = '249acf2_9_123_4.hdf5'
+# old_model_name = 'c5ddc72_9_123_4.hdf5'
+old_model_name = '1d8f4bd_9_123_4.hdf5'
+cut_value = 0.3
 
 random.seed(seed)
 np.random.seed(seed)
@@ -39,7 +48,13 @@ def prediction(model):
     MAE = 0
     test_size = len(test_structures)
     for i in range(test_size):
-        MAE += abs(model.predict_structure(test_structures[i]).ravel() - test_targets[i])
+        model_output = model.predict_structure(test_structures[i]).ravel()
+        err = abs(model_output - test_targets[i])
+        if dump_prediction_cif:
+            name = '{ae}_{mo}_{target}.cif'.format(
+                    ae=err, mo=model_output, target=test_targets[i])
+            test_structures[i].to(filename=name)
+        MAE += err
     MAE /= test_size
     logging.info('MAE is: {mae}'.format(mae=MAE))
 
@@ -67,14 +82,11 @@ tau_dict = {'pbe': 1/0.6279685889089127,
 logging.info('tau_enable={t} and tau_dict is {td}'.format(
     t=str(tau_modify_enable), td=str(tau_dict)))
 
-load_old_model_enable = True
-# old_model_name = '249acf2_9_123_4.hdf5'
-# old_model_name = 'c5ddc72_9_123_4.hdf5'
-old_model_name = '1d8f4bd_9_123_4.hdf5'
-cut_value = 0.3
 
 logging.info('load_old_model_enable={l}, old_model_name={omn}, cut_value={cv}'.format(
     l=load_old_model_enable, omn=old_model_name, cv=cut_value))
+logging.info('predict_before_dataclean={p}, training_new_model={t}'.format(
+    p=predict_before_dataclean, t=training_new_model))
 
 structures = []
 targets = []
@@ -132,6 +144,8 @@ if load_old_model_enable:
     import pickle
     # load the past if needed
     model = MEGNetModel.from_file(old_model_name)
+    if predict_before_dataclean:
+        prediction(model)
     idx = 0
     diff_lst = []
     for i in range(len(s_exp)):
@@ -175,136 +189,136 @@ callback = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=10, res
 for s in test_structures:
     test_input.append(model.graph_converter.graph_to_input(model.graph_converter.convert(s)))
 
-
-if training_mode == 0: # PBE -> HSE ... -> part EXP, one by one
-    idx = 0
-    for i in range(len(data_size)):
-        model.train(structures[idx:idx+data_size[i]], targets[idx:idx+data_size[i]], epochs=ep)
-        idx += data_size[i]
+if training_new_model:
+    if training_mode == 0: # PBE -> HSE ... -> part EXP, one by one
+        idx = 0
+        for i in range(len(data_size)):
+            model.train(structures[idx:idx+data_size[i]], targets[idx:idx+data_size[i]], epochs=ep)
+            idx += data_size[i]
+            prediction(model)
+    elif training_mode == 1: # all training set together
+        model.train(structures, targets, epochs=ep*len(data_size))
         prediction(model)
-elif training_mode == 1: # all training set together
-    model.train(structures, targets, epochs=ep*len(data_size))
-    prediction(model)
-elif training_mode == 2: # only part EXP
-    model.train(structures[sum(data_size[0:len(data_size)-1]):], targets[sum(data_size[0:len(data_size)-1]):], epochs=ep*len(data_size))
-    prediction(model)
-elif training_mode == 3: # all -> all-PBE -> all-PBE-HSE -> ... -> part EXP
-    idx = 0
-    for i in range(len(data_size)):
-        model.train(structures[idx:], targets[idx:], epochs=ep)
-        idx += data_size[i]
+    elif training_mode == 2: # only part EXP
+        model.train(structures[sum(data_size[0:len(data_size)-1]):], targets[sum(data_size[0:len(data_size)-1]):], epochs=ep*len(data_size))
         prediction(model)
-elif training_mode == 4: # use E1 as validation dataset, P -> H -> G -> S one by one
-    idx = 0
-    for i in range(len(data_size)-1):
-        model.train(structures[idx:idx+data_size[i]], targets[idx:idx+data_size[i]],
-                validation_structures=structures[sum(data_size[:-1]):],
-                validation_targets=targets[sum(data_size[:-1]):],
-                callbacks=[callback],
-                epochs=ep,
-                save_checkpoint=False,
-                automatic_correction=False,
-                batch_size = 512)
-        idx += data_size[i]
-        prediction(model)
-elif training_mode == 5: # use more accuracy dataset as validation dataset, P -> H -> G -> S one by one
-    idx = 0
-    for i in range(len(data_size)-1):
-        model.train(structures[idx:idx+data_size[i]], targets[idx:idx+data_size[i]],
-                validation_structures=structures[sum(data_size[:i+1]):],
-                validation_targets=targets[sum(data_size[:i+1]):],
-                callbacks=[callback],
-                epochs=ep,
-                save_checkpoint=False,
-                automatic_correction=False,
-                batch_size = 512)
-        idx += data_size[i]
-        prediction(model)
-elif training_mode == 6: # PBE -> HSE ... -> part EXP, one by one, with 20% validation
-    idx = 0
-    for i in range(len(data_size)):
-        model.train(structures[idx:idx+int(0.8*data_size[i])], targets[idx:idx+int(0.8*data_size[i])],
-                validation_structures=structures[idx+int(0.8*data_size[i]):(idx+data_size[i])],
-                validation_targets=targets[idx+int(0.8*data_size[i]):(idx+data_size[i])],
-                # callbacks=[callback, XiaotongCB((test_input, test_targets), commit_id)],
-                callbacks=[callback],
-                epochs=ep,
-                save_checkpoint=False,
-                batch_size = 512,
-                automatic_correction=False)
-        model.save_model(dump_model_name+'_'+str(i)+'.hdf5')
-        idx += data_size[i]
-        prediction(model)
-elif training_mode == 11: # PBE -> HSE ... -> part EXP, one by one, with 20% of last dataset as validation
-    idx = 0
-    for i in range(len(data_size)):
-        model.train(structures[idx:idx+int(0.8*data_size[i])], targets[idx:idx+int(0.8*data_size[i])],
-                validation_structures=structures[sum(data_size[:-1])+int(0.8*data_size[-1]):],
-                validation_targets=targets[sum(data_size[:-1])+int(0.8*data_size[-1]):],
-                # validation_structures=structures[idx+int(0.8*data_size[i]):(idx+data_size[i])],
-                # validation_targets=targets[idx+int(0.8*data_size[i]):(idx+data_size[i])],
-                # callbacks=[callback, XiaotongCB((test_input, test_targets), commit_id)],
-                callbacks=[callback],
-                epochs=ep,
-                save_checkpoint=False,
-                batch_size = 512,
-                automatic_correction=False)
-        # model.save_model(commit_id+'_'+str(training_mode)+'_'+str(i)+'.hdf5')
-        idx += data_size[i]
-        prediction(model)
-elif training_mode == 7: # all training set together with 20% validation
-    l = len(structures)
-    c = list(zip(structures, targets))
-    random.shuffle(c)
-    structures, targets = zip(*c)
-    model.train(structures[:int(0.8 * l)], targets[:int(0.8 * l)],
-            validation_structures=structures[int(0.8 * l):],
-            validation_targets=targets[int(0.8 * l):],
-            # callbacks=[callback, XiaotongCB((test_input, test_targets), commit_id)],
-            callbacks=[callback],
-            epochs=ep*len(data_size),
-            batch_size = 512,
-            save_checkpoint=False,
-            automatic_correction=False)
-    prediction(model)
-elif training_mode == 8: # only part EXP with 20% validation
-    model.train(structures[-1*data_size[-1]:int(-0.2*data_size[-1])], targets[-1*data_size[-1]:int(-0.2*data_size[-1])],
-            validation_structures=structures[int(-0.2*data_size[-1]):],
-            validation_targets=targets[int(-0.2*data_size[-1]):],
-            # callbacks=[callback, XiaotongCB((test_input, test_targets), commit_id)],
-            callbacks=[callback],
-            epochs=ep*len(data_size),
-            save_checkpoint=False,
-            batch_size = 512,
-            automatic_correction=False)
-    prediction(model)
-elif training_mode == 9 or training_mode == 10: # all -> all-PBE -> all-PBE-HSE -> ... -> part EXP with 20% validation
-    idx = 0
-    for i in range(len(data_size)):
-        s = structures[idx:]
-        t = targets[idx:]
-        sw = sample_weights[idx:]
-        c = list(zip(s, t, sw))
+    elif training_mode == 3: # all -> all-PBE -> all-PBE-HSE -> ... -> part EXP
+        idx = 0
+        for i in range(len(data_size)):
+            model.train(structures[idx:], targets[idx:], epochs=ep)
+            idx += data_size[i]
+            prediction(model)
+    elif training_mode == 4: # use E1 as validation dataset, P -> H -> G -> S one by one
+        idx = 0
+        for i in range(len(data_size)-1):
+            model.train(structures[idx:idx+data_size[i]], targets[idx:idx+data_size[i]],
+                    validation_structures=structures[sum(data_size[:-1]):],
+                    validation_targets=targets[sum(data_size[:-1]):],
+                    callbacks=[callback],
+                    epochs=ep,
+                    save_checkpoint=False,
+                    automatic_correction=False,
+                    batch_size = 512)
+            idx += data_size[i]
+            prediction(model)
+    elif training_mode == 5: # use more accuracy dataset as validation dataset, P -> H -> G -> S one by one
+        idx = 0
+        for i in range(len(data_size)-1):
+            model.train(structures[idx:idx+data_size[i]], targets[idx:idx+data_size[i]],
+                    validation_structures=structures[sum(data_size[:i+1]):],
+                    validation_targets=targets[sum(data_size[:i+1]):],
+                    callbacks=[callback],
+                    epochs=ep,
+                    save_checkpoint=False,
+                    automatic_correction=False,
+                    batch_size = 512)
+            idx += data_size[i]
+            prediction(model)
+    elif training_mode == 6: # PBE -> HSE ... -> part EXP, one by one, with 20% validation
+        idx = 0
+        for i in range(len(data_size)):
+            model.train(structures[idx:idx+int(0.8*data_size[i])], targets[idx:idx+int(0.8*data_size[i])],
+                    validation_structures=structures[idx+int(0.8*data_size[i]):(idx+data_size[i])],
+                    validation_targets=targets[idx+int(0.8*data_size[i]):(idx+data_size[i])],
+                    # callbacks=[callback, XiaotongCB((test_input, test_targets), commit_id)],
+                    callbacks=[callback],
+                    epochs=ep,
+                    save_checkpoint=False,
+                    batch_size = 512,
+                    automatic_correction=False)
+            model.save_model(dump_model_name+'_'+str(i)+'.hdf5')
+            idx += data_size[i]
+            prediction(model)
+    elif training_mode == 11: # PBE -> HSE ... -> part EXP, one by one, with 20% of last dataset as validation
+        idx = 0
+        for i in range(len(data_size)):
+            model.train(structures[idx:idx+int(0.8*data_size[i])], targets[idx:idx+int(0.8*data_size[i])],
+                    validation_structures=structures[sum(data_size[:-1])+int(0.8*data_size[-1]):],
+                    validation_targets=targets[sum(data_size[:-1])+int(0.8*data_size[-1]):],
+                    # validation_structures=structures[idx+int(0.8*data_size[i]):(idx+data_size[i])],
+                    # validation_targets=targets[idx+int(0.8*data_size[i]):(idx+data_size[i])],
+                    # callbacks=[callback, XiaotongCB((test_input, test_targets), commit_id)],
+                    callbacks=[callback],
+                    epochs=ep,
+                    save_checkpoint=False,
+                    batch_size = 512,
+                    automatic_correction=False)
+            # model.save_model(commit_id+'_'+str(training_mode)+'_'+str(i)+'.hdf5')
+            idx += data_size[i]
+            prediction(model)
+    elif training_mode == 7: # all training set together with 20% validation
+        l = len(structures)
+        c = list(zip(structures, targets))
         random.shuffle(c)
-        s, t, sw = zip(*c)
-        l = len(s)
-        if training_mode == 9:
-            sw = None
-        model.train(s[:int(0.8*l)], t[:int(0.8*l)],
-                validation_structures=s[int(0.8*l):],
-                validation_targets=t[int(0.8*l):],
+        structures, targets = zip(*c)
+        model.train(structures[:int(0.8 * l)], targets[:int(0.8 * l)],
+                validation_structures=structures[int(0.8 * l):],
+                validation_targets=targets[int(0.8 * l):],
                 # callbacks=[callback, XiaotongCB((test_input, test_targets), commit_id)],
                 callbacks=[callback],
-                save_checkpoint=False,
-                automatic_correction=False,
-                sample_weights=sw,
+                epochs=ep*len(data_size),
                 batch_size = 512,
-                epochs=ep)
-        model.save_model(dump_model_name+'_'+str(i)+'.hdf5')
-        idx += data_size[i]
+                save_checkpoint=False,
+                automatic_correction=False)
         prediction(model)
-else:
-    pass
+    elif training_mode == 8: # only part EXP with 20% validation
+        model.train(structures[-1*data_size[-1]:int(-0.2*data_size[-1])], targets[-1*data_size[-1]:int(-0.2*data_size[-1])],
+                validation_structures=structures[int(-0.2*data_size[-1]):],
+                validation_targets=targets[int(-0.2*data_size[-1]):],
+                # callbacks=[callback, XiaotongCB((test_input, test_targets), commit_id)],
+                callbacks=[callback],
+                epochs=ep*len(data_size),
+                save_checkpoint=False,
+                batch_size = 512,
+                automatic_correction=False)
+        prediction(model)
+    elif training_mode == 9 or training_mode == 10: # all -> all-PBE -> all-PBE-HSE -> ... -> part EXP with 20% validation
+        idx = 0
+        for i in range(len(data_size)):
+            s = structures[idx:]
+            t = targets[idx:]
+            sw = sample_weights[idx:]
+            c = list(zip(s, t, sw))
+            random.shuffle(c)
+            s, t, sw = zip(*c)
+            l = len(s)
+            if training_mode == 9:
+                sw = None
+            model.train(s[:int(0.8*l)], t[:int(0.8*l)],
+                    validation_structures=s[int(0.8*l):],
+                    validation_targets=t[int(0.8*l):],
+                    # callbacks=[callback, XiaotongCB((test_input, test_targets), commit_id)],
+                    callbacks=[callback],
+                    save_checkpoint=False,
+                    automatic_correction=False,
+                    sample_weights=sw,
+                    batch_size = 512,
+                    epochs=ep)
+            model.save_model(dump_model_name+'_'+str(i)+'.hdf5')
+            idx += data_size[i]
+            prediction(model)
+    else:
+        pass
 
 ## model save and load
 # model.save_model(commit_id+str(training_mode)+'.hdf5')
