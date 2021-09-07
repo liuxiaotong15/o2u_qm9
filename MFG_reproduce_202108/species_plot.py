faba20d671e52a3a1f9afd8294b114879106cb6d
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2021-09-07 15:14:01 +0800

    add species bar script
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

---
+++ MFG_reproduce_202108/species_plot.py
@@ -0,0 +1,83 @@
+gllb = "{Element O: 800, Element S: 327, Element K: 286, Element F: 253, Element P: 240, Element Se: 237, Element Na: 233, Element Ba: 182, Element Cl: 176, Element Li: 176, Element Rb: 169, Element As: 159, Element Sr: 155, Element Cs: 153, Element Te: 151, Element N: 140, Element Ca: 129, Element Br: 127, Element Ge: 119, Element Cu: 118, Element I: 118, Element Sb: 117, Element Si: 110, Element Al: 108, Element Tl: 102, Element Ag: 100, Element Sn: 99, Element B: 98, Element Zn: 97, Element Cd: 91, Element Mg: 89, Element Ga: 82, Element Bi: 78, Element In: 78, Element Au: 75, Element Ta: 66, Element La: 63, Element Hg: 63, Element Nb: 62, Element Pt: 62, Element Pb: 61, Element Zr: 53, Element Y: 51, Element V: 50, Element Pd: 48, Element Sc: 46, Element C: 44, Element Be: 40, Element Ti: 38, Element Mo: 34, Element Hf: 34, Element W: 31, Element Fe: 22, Element Rh: 16, Element Re: 16, Element H: 15, Element Co: 14, Element Ru: 13, Element Cr: 12, Element Os: 10, Element Ir: 10, Element Ni: 9, Element Mn: 6}"
+
+hse = "{Element O: 2365, Element Li: 871, Element F: 529, Element P: 449, Element S: 439, Element Mn: 431, Element Na: 340, Element N: 338, Element C: 318, Element Ti: 315, Element Cu: 310, Element Si: 309, Element Ni: 304, Element Fe: 303, Element V: 300, Element K: 296, Element Ba: 285, Element Ca: 280, Element Cr: 277, Element Al: 272, Element Sn: 272, Element Mg: 272, Element Sr: 262, Element Sb: 257, Element Rb: 244, Element Co: 241, Element Te: 240, Element La: 229, Element Se: 227, Element Zr: 220, Element H: 219, Element As: 217, Element Ag: 217, Element Zn: 215, Element Ge: 210, Element Y: 210, Element B: 202, Element Sc: 196, Element Bi: 175, Element Nb: 173, Element Ga: 171, Element Pt: 160, Element Au: 155, Element Pd: 153, Element Rh: 151, Element Cd: 148, Element Tl: 145, Element Hg: 136, Element Ta: 134, Element Mo: 131, Element I: 128, Element Cs: 126, Element Hf: 122, Element In: 115, Element Lu: 107, Element Ir: 106, Element Be: 106, Element W: 100, Element Br: 99, Element Pb: 98, Element Ru: 98, Element Re: 88, Element Os: 47, Element Cl: 39, Element Tc: 37, Element Yb: 6, Element Xe: 4, Element He: 3, Element Nd: 2, Element Kr: 2, Element Ho: 1, Element Pa: 1, Element Sm: 1, Element Tb: 1, Element Eu: 1}"
+
+scan = "{Element S: 111, Element O: 87, Element Se: 77, Element Cu: 61, Element Sb: 47, Element Te: 46, Element Ag: 40, Element Sn: 38, Element I: 35, Element In: 35, Element P: 35, Element Zn: 33, Element Bi: 32, Element Cd: 31, Element As: 31, Element Br: 30, Element Cs: 28, Element Ba: 27, Element Ge: 24, Element Ga: 24, Element K: 23, Element Cl: 23, Element Tl: 22, Element Sr: 21, Element Si: 20, Element Pb: 17, Element N: 17, Element Hg: 16, Element Rb: 16, Element Li: 16, Element Ca: 15, Element Al: 15, Element F: 14, Element Na: 14, Element Mg: 11, Element La: 11, Element Ta: 10, Element Ti: 10, Element B: 9, Element Au: 8, Element Sc: 8, Element Th: 7, Element C: 7, Element Mo: 7, Element Y: 6, Element V: 5, Element Pd: 5, Element Cr: 4, Element Hf: 4, Element Be: 3, Element Os: 3, Element Zr: 3, Element W: 3, Element Nb: 3, Element Ru: 3, Element Pt: 2, Element Fe: 2, Element Re: 2, Element Ni: 2, Element H: 2, Element Ne: 1, Element Ar: 1, Element Xe: 1, Element Co: 1, Element Yb: 1, Element Kr: 1}"
+
+pbe = "{Element O: 28312, Element Li: 9741, Element P: 6519, Element F: 5231, Element S: 4729, Element Fe: 4246, Element H: 4191, Element Mn: 4164, Element Si: 3664, Element N: 3581, Element C: 3438, Element V: 3360, Element Na: 3264, Element Cu: 3076, Element Co: 2789, Element K: 2666, Element B: 2638, Element Ba: 2637, Element Ni: 2621, Element Se: 2515, Element Ti: 2408, Element Cl: 2386, Element Ca: 2354, Element Al: 2324, Element Cr: 2317, Element Te: 2270, Element Mg: 2180, Element Sb: 2118, Element Sn: 2113, Element Zn: 1964, Element W: 1937, Element Sr: 1827, Element Ge: 1764, Element Mo: 1731, Element Bi: 1633, Element As: 1543, Element Rb: 1527, Element La: 1499, Element Nb: 1463, Element Y: 1316, Element Ag: 1315, Element In: 1286, Element I: 1282, Element Ga: 1262, Element Cs: 1128, Element Tl: 1095, Element Br: 1047, Element Cd: 1035, Element Zr: 1000, Element Pd: 984, Element Pb: 980, Element Nd: 921, Element Ta: 910, Element Au: 859, Element Pr: 841, Element Sm: 835, Element Rh: 811, Element Hg: 789, Element Pt: 787, Element Ru: 750, Element Ho: 739, Element Sc: 735, Element U: 718, Element Ce: 718, Element Yb: 683, Element Dy: 674, Element Er: 673, Element Ir: 633, Element Hf: 621, Element Tb: 593, Element Tm: 543, Element Re: 509, Element Lu: 502, Element Be: 454, Element Os: 366, Element Th: 365, Element Gd: 328, Element Eu: 280, Element Tc: 188, Element Pu: 132, Element Np: 131, Element Pm: 122, Element Pa: 81, Element Ac: 77, Element Xe: 56, Element Kr: 8, Element He: 6}"
+
+exp = "{Species O2-: 431, Species S2-: 318, Species Se2-: 247, Species Si0+: 146, Species Ge0+: 132, Species Ba2+: 123, Species Al0+: 121, Species K+: 119, Species Ni0+: 111, Species B0+: 101, Species In3+: 101, Species Cs+: 100, Species Te2-: 99, Species Cu+: 98, Species In0+: 85, Species Pd0+: 83, Species Co0+: 83, Species Cd2+: 80, Species Bi3+: 79, Species Rb+: 76, Species Sn0+: 74, Species Ga3+: 73, Species Na+: 70, Species Ag+: 69, Species B3+: 69, Species Sb3+: 69, Species Pt0+: 68, Species Rh0+: 67, Species Ge4+: 66, Species Cu0+: 64, Species Ga0+: 63, Species Zn2+: 62, Species Sb0+: 61, Species C0+: 61, Species Cl-: 59, Species Fe0+: 59, Species P0+: 59, Species Mg0+: 59, Species I-: 58, Species Zr0+: 57, Species Mn0+: 55, Species Li+: 54, Species Tl+: 53, Species Ir0+: 51, Species Ce0+: 51, Species P5+: 50, Species Au0+: 50, Species F-: 50, Species Te4+: 48, Species Sn4+: 48, Species Br-: 47, Species Hg2+: 47, Species Nd0+: 46, Species Ca0+: 45, Species As0+: 44, Species La0+: 43, Species Ag0+: 43, Species Sc0+: 42, Species Zn0+: 42, Species Y0+: 42, Species Yb0+: 42, Species Hf0+: 42, Species Se0+: 41, Species Tb0+: 40, Species Sm0+: 40, Species Dy0+: 40, Species Se4+: 39, Species Te0+: 39, Species Pb2+: 38, Species Ru0+: 38, Species Ho0+: 38, Species Ti0+: 38, Species La3+: 38, Species As3-: 38, Species N3-: 37, Species Eu2+: 36, Species Er0+: 35, Species N0+: 34, Species As3+: 34, Species Sm3+: 34, Species Mo6+: 34, Species Bi0+: 33, Species U0+: 33, Species Sr2+: 32, Species Pr0+: 32, Species O0+: 32, Species Ba0+: 30, Species Tm0+: 30, Species P3-: 30, Species Cd0+: 29, Species Li0+: 29, Species S0+: 29, Species Sr0+: 28, Species Ca2+: 28, Species V5+: 27, Species Tl0+: 27, Species Pb0+: 27, Species Al3+: 26, Species Th0+: 26, Species Sb3-: 25, Species Mg2+: 25, Species Nb5+: 25, Species Eu0+: 24, Species Yb3+: 24, Species Hg0+: 24, Species Cu2+: 24, Species S-: 23, Species V0+: 23, Species Si4+: 23, Species Ce3+: 23, Species P4+: 23, Species Y3+: 23, Species Lu0+: 23, Species Nd3+: 22, Species Gd0+: 20, Species Cr0+: 20, Species I5+: 20, Species Os0+: 19, Species Gd3+: 19, Species Se-: 18, Species Dy3+: 18, Species Mn2+: 18, Species Ti4+: 18, Species Sc3+: 17, Species Ta5+: 17, Species Eu3+: 17, Species Pd2+: 16, Species Cr3+: 16, Species Na0+: 16, Species Zr4+: 16, Species H+: 15, Species Nb0+: 15, Species Pr3+: 15, Species Mo0+: 14, Species Er3+: 14, Species Sb5+: 14, Species Ta0+: 14, Species Be0+: 13, Species Th4+: 12, Species Ho3+: 12, Species Tb3+: 12, Species W6+: 12, Species Cs0+: 12, Species U4+: 11, Species Fe2+: 11, Species Sn2+: 11, Species Re0+: 10, Species Hf4+: 10, Species K0+: 10, Species Fe3+: 9, Species Lu3+: 9, Species Ni2+: 9, Species Co3+: 9, Species W0+: 9, Species Ti3+: 9, Species Tl3+: 8, Species Ge4-: 8, Species H-: 8, Species N5+: 8, Species Sb2-: 7, Species I0+: 7, Species P2-: 7, Species Rb0+: 7, Species C4-: 7, Species Ge2+: 6, Species Mn3+: 6, Species Mn4+: 6, Species Te-: 6, Species As5+: 6, Species S6+: 6, Species Cl0+: 6, Species P-: 6, Species Re4+: 6, Species Co2+: 6, Species As2-: 5, Species Ru2+: 5, Species H0+: 5, Species Ru4+: 5, Species V4+: 5, Species Be2+: 5, Species Mo4+: 5, Species Hg+: 5, Species Tm3+: 5, Species Ag2+: 5, Species As-: 5, Species V3+: 5, Species Au+: 5, Species Ni3+: 5, Species Nb4+: 5, Species Rh3+: 5, Species Pb4+: 5, Species Os2+: 4, Species Yb2+: 4, Species Re5+: 4, Species Br0+: 4, Species Pr4+: 4, Species C2-: 4, Species Te6+: 4, Species Mo5+: 4, Species Rh4+: 4, Species C4+: 4, Species Ge2-: 4, Species F0+: 4, Species Tc0+: 4, Species B3-: 4, Species Cr6+: 4, Species Si4-: 4, Species Ti2+: 4, Species Ru3+: 3, Species Ta3+: 3, Species Pt4+: 3, Species Au3+: 3, Species Fe2.5+: 3, Species N-: 3, Species Ir4+: 3, Species Ir3+: 3, Species Cr4+: 3, Species In2+: 3, Species Ga2+: 3, Species Co2.5+: 3, Species B0.5-: 3, Species Gd4+: 2, Species C1.5-: 2, Species Ce4+: 2, Species Cr2+: 2, Species Cu2.5+: 2, Species Sm2+: 2, Species As2+: 2, Species Nb2.5+: 2, Species Nd2+: 2, Species W2+: 2, Species In+: 2, Species Se2.33-: 2, Species P1.5-: 2, Species Sb1.5-: 2, Species Ge3+: 2, Species Se0.5-: 2, Species Tb2+: 2, Species As2.5-: 2, Species Re6+: 2, Species C-: 2, Species Zr2+: 2, Species V2+: 2, Species B-: 2, Species Os3+: 2, Species W5+: 2, Species Re7+: 2, Species Os5+: 2, Species Os5.5+: 2, Species Ru5+: 2, Species C3-: 2, Species Ir2+: 2, Species P3+: 2, Species Ag3+: 1, Species Ca1.8+: 1, Species Tl2+: 1, Species Tb+: 1, Species Sc2+: 1, Species Cu0.9+: 1, Species Xe4+: 1, Species B1.75-: 1, Species Nd4+: 1, Species Nb2.714+: 1, Species Sm2.17+: 1, Species Eu2.67+: 1, Species Cl7+: 1, Species N0.33-: 1, Species Te0.667-: 1, Species U6+: 1, Species Pr2+: 1, Species Hf1.71+: 1, Species Th3+: 1, Species Sb2.5-: 1, Species Sm2.5+: 1, Species I0.5+: 1, Species B0.286-: 1, Species Sn0.5-: 1, Species Sb3.4-: 1, Species Nb3+: 1, Species Os6+: 1, Species Ti2.5+: 1, Species Pd4+: 1, Species In3-: 1, Species O-: 1, Species Mo1.952+: 1, Species Sb3.5+: 1, Species U3.333+: 1, Species Re5.5+: 1, Species Se1.75-: 1, Species Rh1.6+: 1, Species La2+: 1, Species P0.5-: 1, Species Se0.333-: 1, Species Ta3.75+: 1, Species In1.5+: 1, Species Re1.25+: 1, Species Ga1.5+: 1, Species C2+: 1, Species Sc2.2+: 1, Species In2.5+: 1, Species Ge3-: 1, Species Co+: 1, Species Te+: 1, Species Co4+: 1, Species I7+: 1, Species Ni2.5+: 1, Species U4.5+: 1, Species Nb3.33+: 1, Species Co2.6667+: 1, Species Sm2.67+: 1, Species La2.75+: 1, Species V0.25+: 1, Species Nb1.667+: 1, Species Tb4+: 1, Species B0.333-: 1, Species Tc4+: 1, Species Cu1.5+: 1, Species U2+: 1, Species Ni1.4+: 1, Species La2.5+: 1, Species Bi3-: 1, Species Tl-: 1, Species Se+: 1, Species U3.5+: 1, Species Sn2-: 1, Species Rb0.33+: 1, Species Cu1.67+: 1, Species P1.25-: 1, Species Sn3.5+: 1, Species Dy4+: 1, Species Mo2.4+: 1, Species W3+: 1, Species Pb3.5+: 1, Species As2.33+: 1, Species Bi2+: 1, Species P0.5+: 1, Species Zr3+: 1, Species Cu3+: 1, Species Gd2+: 1, Species N2-: 1, Species Ge+: 1, Species U2.67+: 1, Species N3+: 1, Species U5+: 1, Species B0.25-: 1, Species Pt2+: 1, Species Nd2.67+: 1, Species Mo3+: 1, Species Cu1.11+: 1, Species Ni+: 1, Species B0.067-: 1, Species N0.5-: 1, Species Cu2.4+: 1, Species Mo2+: 1, Species W4+: 1, Species Bi0.25+: 1, Species Mn6+: 1, Species Ta3.6+: 1, Species Bi5+: 1, Species V3.5+: 1, Species Ge3.5+: 1, Species Pd1.67+: 1, Species Rh3.5+: 1, Species Se0.667-: 1, Species S1.2-: 1, Species Nb1.5+: 1, Species Cr1.5+: 1, Species V2.615+: 1, Species Ta1.5+: 1, Species B0.75-: 1, Species As1.5-: 1, Species Ru7+: 1}"
+import periodictable
+import re
+
+def species_str2dict(re_str, species_str):
+    exp_re = re.findall(re_str, species_str)
+    exp_dict = {}
+    for i, j in exp_re:
+        if i in exp_dict.keys():
+            exp_dict[i] += int(j)
+        else:
+            exp_dict[i] = int(j)
+    return exp_dict
+
+exp_dict = species_str2dict('Species\s*([A-Za-z]*)\S*:\s*(\d*)', exp)
+print(len(exp_dict))
+
+ele_str = 'Element\s*([A-Za-z]*)\S*:\s*(\d*)'
+pbe_dict = species_str2dict(ele_str, pbe)
+gllb_dict = species_str2dict(ele_str, gllb)
+hse_dict = species_str2dict(ele_str, hse)
+scan_dict = species_str2dict(ele_str, scan)
+
+el_lst = []
+for el in periodictable.elements: 
+    if el.symbol in set(list(exp_dict.keys()) + list(pbe_dict.keys()) + list(gllb_dict.keys()) + list(hse_dict.keys()) + list(scan_dict.keys())):
+        el_lst.append(el.symbol)
+
+print(el_lst)
+
+exp_y = []
+for e in el_lst:
+    if e in exp_dict.keys():
+        exp_y.append(exp_dict[e])
+    else:
+        exp_y.append(0)
+ 
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(len(el_lst))  # the label locations
+width = 0.15  # the width of the bars
+
+fig, ax = plt.subplots()
+ax.bar(x - width*2, np.array(exp_y)/max(exp_y), width, label='EXP')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+# ax.set_ylabel('Scores')
+# ax.set_title('Scores by group and gender')
+ax.set_xticks(x)
+ax.set_xticklabels(el_lst, rotation = 75)# , ha="right")
+
+# ax.bar_label(rects1, padding=3)
+# ax.bar_label(rects2, padding=3)
+
+dft_str = ['gllb', 'pbe', 'hse', 'scan']
+for idx, ss in enumerate([gllb, pbe, hse, scan]):
+    dft_dict = species_str2dict(ele_str, ss)
+    print(len(dft_dict))
+    dft_y = []
+    for e in el_lst:
+        if e in dft_dict.keys():
+            dft_y.append(dft_dict[e])
+        else:
+            dft_y.append(0)
+
+    ax.bar(x + width*(idx-1), np.array(dft_y)/max(dft_y), width, label=dft_str[idx])
+ 
+
+
+ax.legend()
+fig.tight_layout()
+plt.show()
+
