a8e5e13fc97c4aafa425935bbc4ad8596f808ca6
liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>
2021-09-30 10:56:15 +0800

    update log analysis script
    Signed-off-by: liuxiaotong15 <liuxiaotong15@mails.ucas.ac.cn>

--- MFG_reproduce_202108/log_analysis.py
+++ MFG_reproduce_202108/log_analysis.py
@@ -7,6 +7,7 @@ filenames = ["80c5815_0_123.log", "fe977d4_0_123.log", "6a33607_0_123.log", "e19
 for filename in filenames:
     print(root+filename)
     value_dict = {'E':[], 'P':[], 'G':[], 'H':[], 'S':[],}
+    subtree_value_dict = {'GPHS':[], 'EPHS':[], 'EGHS':[], 'EGPS':[], 'EGPH':[],}
     all_number_lst = []
     with open(root+filename, 'r') as f:
         for line in f.readlines():
@@ -20,9 +21,18 @@ for filename in filenames:
                 match_obj = re.match(r'.*_['+key+r']\ is:\ \[(.*)\]', line.strip())
                 if match_obj:
                     value_dict[key].append(float(match_obj.group(1)))
+
+            for key in subtree_value_dict.keys():
+                match_obj = re.match(r'.*_'+key+r'_.*\ is:\ \[(.*)\]', line.strip())
+                if match_obj:
+                    subtree_value_dict[key].append(float(match_obj.group(1)))
+
     print('Ending with char test:')
     for key in value_dict.keys():
         print(key, np.mean(value_dict[key]))
+    print('Subtree analysis:')
+    for key in subtree_value_dict.keys():
+        print(key, np.mean(subtree_value_dict[key]))
     print('All data analysis:')
     print('Max: ', max(all_number_lst))
     print('Min: ', min(all_number_lst))
