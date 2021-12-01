fa8524b18ff3f5402cd86af0d54f554170d3adb8
xiaotong.liu <liuxiaotong@bistu.edu.cn>
2021-12-01 08:31:20 +0800

    best next round
    Signed-off-by: xiaotong.liu <liuxiaotong@bistu.edu.cn>

--- MFG_reproduce_202108/onion_training.py
+++ MFG_reproduce_202108/onion_training.py
@@ -33,7 +33,7 @@ if training_mode in [0, 1]:
     swap_E1_test = bool(training_mode&1)
     # special_path = 'init_randomly_EGPHS_EGPH_EGP_EG_E'  # worst1
     special_path = 'init_randomly_EGPHS_EPHS_EHS_EH_E'  # best
-    last_commit_id = '3a74836'
+    last_commit_id = '1706f35'
     if training_mode == 0:
         old_model_name = last_commit_id + '_0_123_' + special_path + '.hdf5'
         GPU_device = "0"
