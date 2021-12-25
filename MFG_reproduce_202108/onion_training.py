223d078ca46161a073ca887ac8e379a202352f3a
xiaotong.liu <liuxiaotong@bistu.edu.cn>
2021-12-25 20:22:23 +0800

    after 2ef6508 +1 round
    Signed-off-by: xiaotong.liu <liuxiaotong@bistu.edu.cn>

--- MFG_reproduce_202108/onion_training.py
+++ MFG_reproduce_202108/onion_training.py
@@ -20,7 +20,7 @@ seed = 123
 GPU_seed = 11111
 GPU_device = "0"
 dump_prediction_cif = False
-load_old_model_enable = False
+load_old_model_enable = True
 predict_before_dataclean = False
 training_new_model = True
 contain_e1_in_every_node = False
@@ -34,7 +34,7 @@ if training_mode in [0, 1]:
     # special_path = 'init_randomly_EGPHS_EGPH_EGP_EG_E'  # worst1
     # special_path = 'init_randomly_EGPHS_EPHS_EPH_EH_E'  # better
     special_path = 'init_randomly_EGPHS_EPHS_EHS_EH_E'  # best
-    last_commit_id = '316a10e'
+    last_commit_id = '2ef6508'
     if training_mode == 0:
         old_model_name = last_commit_id + '_0_123_' + special_path + '.hdf5'
         GPU_device = "0"
